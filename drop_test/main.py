94f90f9c6c55de714c2a3155f9e354ffe1e57fa4
Kis Bálint <kis.balint0z0@gmail.com>
2026-04-18 11:55:23 +0200

    Solve drop test

--- drop_test/main.py
+++ drop_test/main.py
@@ -3,7 +3,23 @@ from pathlib import Path
 
 def main():
     data = Path("input.txt").read_text(encoding="utf-8")
-    print(data, end="")
+    lines = data.splitlines()
+
+    for line in lines:
+        information = line.split(", ")
+        N, H = int(information[0]), int(information[1])
+        print(min_num_of_drops(N, H))
+
+def min_num_of_drops(N: int, H: int) -> int:
+    dp = [0] * (N + 1)
+    drops = 0
+
+    while dp[N] < H:
+        drops += 1
+        for i in range(N, 0, -1):
+            dp[i] = dp[i] + dp[i - 1] + 1
+
+    return drops
 
 
 if __name__ == "__main__":
